082e9dc1b787e928ef3facac35dd3af16f38c98a
Hiury Lima <limahiury6@gmail.com>
2025-07-20 15:34:36 -0300

    Desafio 004: Conversor de Moedas avançado

---
+++ desafios/d004/conversao.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 04</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+            $cotacao = 5.17;
+
+            $real = $_GET['dinheiro'] ?? 0;
+
+            $dolar = $real / $cotacao;
+            
+            // Utilizando a biblioteca intl (Internallization PHP) de internacionalização
+            $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+            echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong ></p>";
+        ?>
+        <button onclick="javascript:history.go(-1)">&#x2B05 Voltar</button>
+    </main>
+</body>
+</html>
